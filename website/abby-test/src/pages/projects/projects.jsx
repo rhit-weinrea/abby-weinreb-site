38656e9f022577264554d6dd24e0c9985b866144
rhit-weinrea <101138434+rhit-weinrea@users.noreply.github.com>
2025-09-16 01:54:15 -0400

    . error

--- website/abby-test/src/pages/projects/projects.jsx
+++ website/abby-test/src/pages/projects/projects.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Project from "../../components/project/project";
 import styles from "./projects.module.css";
-import r1 from "/resources/rethink1.jpeg" 
-import r2 from "/resources/rethink2.jpeg"
-import r3 from "/resources/rethink3.jpeg"
-import g1 from "/resources/garp1.png"
-import g2 from "/resources/garp2.png"   
+import r1 from "./resources/rethink1.jpeg" 
+import r2 from "./resources/rethink2.jpeg"
+import r3 from "./resources/rethink3.jpeg"
+import g1 from "./resources/garp1.png"
+import g2 from "./resources/garp2.png"   
 
 const reTHinkImages = [
 
